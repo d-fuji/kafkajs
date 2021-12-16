5d1495e1ba5af0a7fb8b0242914e85ab19771847
daiki-fujii <d0a5i16ki@gmail.com>
2021-12-16 22:59:35 +0900

    delete error handling

--- consumer.js
+++ consumer.js
@@ -27,30 +27,4 @@ const run = async () => {
   })
 }
 
-run().catch(e => console.error(`[example/consumer] ${e.message}`, e))
-
-const errorTypes = ['unhandledRejection', 'uncaughtException']
-const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
-
-errorTypes.map(type => {
-  process.on(type, async e => {
-    try {
-      console.log(`process.on ${type}`)
-      console.error(e)
-      await consumer.disconnect()
-      process.exit(0)
-    } catch (_) {
-      process.exit(1)
-    }
-  })
-})
-
-signalTraps.map(type => {
-  process.once(type, async () => {
-    try {
-      await consumer.disconnect()
-    } finally {
-      process.kill(process.pid, type)
-    }
-  })
-})
+run()
--- producer.js
+++ producer.js
@@ -37,29 +37,4 @@ const run = async () => {
   setInterval(sendMessage, 3000)
 }
 
-run().catch(e => console.error(`[example/producer] ${e.message}`, e))
-
-const errorTypes = ['unhandledRejection', 'uncaughtException']
-const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
-
-errorTypes.map(type => {
-  process.on(type, async () => {
-    try {
-      console.log(`process.on ${type}`)
-      await producer.disconnect()
-      process.exit(0)
-    } catch (_) {
-      process.exit(1)
-    }
-  })
-})
-
-signalTraps.map(type => {
-  process.once(type, async () => {
-    try {
-      await producer.disconnect()
-    } finally {
-      process.kill(process.pid, type)
-    }
-  })
-})
+run()
